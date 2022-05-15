7e987b4ba66f0520f06a0941f9d31060e8062e76
thomerson <756155037@qq.com>
2022-05-15 14:22:03 +0800

    update

---
+++ Gatlin.Utils.Core/Gatlin.Utils.Core.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Gatlin.Utils.Core.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
